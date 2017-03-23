cd216748a784e1f8760da22ec93e756f54e809f8
Anton Shulgin <anton.shulgin@gmx.com>
2017-03-23 13:07:46 +0300

    added setAppData

--- src/features/user.js
+++ src/features/user.js
@@ -12,8 +12,28 @@
 		const util = glomeApiService.util;
 
 		externals.getUser = getUser;
+		externals.setAppData = setAppData;
 
 		return externals;
+
+		function setAppData(userId, appData) {
+			if (!util.isNonEmptyString(userId)) {
+				return util.panic('No valid userId provided');
+			}
+			if (!util.isNonEmptyObject(appData)) {
+				return util.panic('No valid appData provided');
+			}
+			const params = {
+				method: 'post', // `put` in the example from the doc
+				path: '/user/' + userId + '/data',
+				includeHeaders: {
+					appId: true,
+					appSecret: true
+				},
+				data: appData
+			};
+			return core.produceRequest(params);
+		}
 
 		function getUser(userId) {
 			if (!util.isNonEmptyString(userId)) {
