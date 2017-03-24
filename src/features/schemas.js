a1eedf75e19e7d9393bcf5477d3a72c39fc9cbd5
Anton Shulgin <anton.shulgin@gmx.com>
2017-03-24 12:00:03 +0300

    added createSchema

--- src/features/schemas.js
+++ src/features/schemas.js
@@ -11,9 +11,25 @@
 		const core = glomeApiService.core;
 		const util = glomeApiService.util;
 
+		externals.createSchema = createSchema;
+
 		return externals;
+
+		function createSchema(schemaModel) {
+			if (!util.isNonEmptyObject(schemaModel)) {
+				return core.panic('No valid schemaModel provided');
+			}
+			const params = {
+				method: 'post',
+				path: '/schemas',
+				includeHeaders: {
+					appId: true,
+					appSecret: true
+				},
+				data: schemaModel
+			};
+			return core.produceRequest(params);
+		}
 	}
 
 })(this.glomeApiService);
-
-
