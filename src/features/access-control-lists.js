e88999e1672bce27fbbf423672daabf465aba20d
Anton Shulgin <anton.shulgin@gmx.com>
2017-03-24 13:31:06 +0300

    added getList

--- src/features/access-control-lists.js
+++ src/features/access-control-lists.js
@@ -11,7 +11,27 @@
 		const core = glomeApiService.core;
 		const util = glomeApiService.util;
 
+		externals.getList = getList;
+
 		return externals;
+
+		function getList(schemaName, socialObjectId) {
+			if (!util.isNonEmptyString(schemaName)) {
+				return core.panic('No valid schemaName provided');
+			}
+			if (!util.isNonEmptyString(socialObjectId)) {
+				return core.panic('No valid socialObjectId provided');
+			}
+			const params = {
+				method: 'get',
+				path: '/schemas/' + encodeURIComponent(schemaName) + '/' + encodeURIComponent('socialObjectId') + '/acl',
+				includeHeaders: {
+					appId: true,
+					appSecret: true
+				}
+			};
+			return core.produceRequest(params);
+		}
 	}
 
 })(this.glomeApiService);
